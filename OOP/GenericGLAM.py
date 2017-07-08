7be9cef1e302be8fcf4bb38881898e284c6d85d3
infobliss <sid.nitdurgapur@gmail.com>
2017-07-08 08:58:11 +0530

    Modified code using OOP design

---
+++ OOP/GenericGLAM.py
@@ -0,0 +1,15 @@
+from infobox_templates import photograph_template, art_photo_template
+
+
+class GenericGLAM:
+    
+    def __init__(self, template):
+        self.template = template;
+
+    def fill_template(parameters, right_template):
+        #fill the template based on the values provided by the derived GLAM class       
+        if right_tempate == 1:
+            #for right_tempate = 1; Assume that template is photograph template
+            wikitext = photograph_template.format(**parameters)
+            #To be displayed to the user for editing
+            print wikitext
