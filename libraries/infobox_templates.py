3e723bd8b627383c04575fee8e6bd1eb6549f3cc
infobliss <sid.nitdurgapur@gmail.com>
2017-07-28 18:41:11 +0530

    file hierarchy restructured

--- libraries/infobox_templates.py
+++ libraries/infobox_templates.py
@@ -1,28 +1,30 @@
-art_photo_parameters = {'artist': '',
-                        'title': '',
-                        'description': '',
-                        'date': '',
-                        'medium': '',
-                        'dimensions': '',
-                        'institution': '',
-                        'location': '',
-                        'references': '',
-                        'object_history': '',
-                        'exhibition_history': '',
-                        'credit_line': '',
-                        'inscriptions': '',
-                        'notes': '',
-                        'accession_number': '',
-                        'artwork_license': '',
-                        'place_of_creation': '',
-                        'photo_description': '',
-                        'photo_date': '',
-                        'photographer': '',
-                        'source': '',
-                        'photo_license': '',
-                        'other_versions': ''}
+art_photo_parameters = {
+      'artist': '',
+      'title': '',
+      'description': '',
+      'date': '',
+      'medium': '',
+      'dimensions': '',
+      'institution': '',
+      'location': '',
+      'references': '',
+      'object_history': '',
+      'exhibition_history': '',
+      'credit_line': '',
+      'inscriptions': '',
+      'notes': '',
+      'accession_number': '',
+      'artwork_license': '',
+      'place_of_creation': '',
+      'photo_description': '',
+      'photo_date': '',
+      'photographer': '',
+      'source': '',
+      'photo_license': '',
+      'other_versions': ''
+}
 
-art_photo_template = '''
+art_photo_template = '''\
 {{{{Art Photo
  |artist             = {artist}
  |title              = {title}
@@ -50,10 +52,55 @@ art_photo_template = '''
 }}}}
 '''
 
+photograph_parameters = {
+      'photographer': '',
+      'title': '',
+      'description': '',
+      'depicted people': '',
+      'depicted place': '',
+      'date': '',
+      'medium': '',
+      'dimensions': '',
+      'institution': '',
+      'department': '',
+      'references': '',
+      'object_history': '',
+      'exhibition_history': '',
+      'credit_line': '',
+      'inscriptions': '',
+      'notes': '',
+      'accession_number': '',
+      'source': '',
+      'permission': '',
+      'other_versions': '',
+      'wikidata': '',
+      'camera coord': ''
+ }
 
-def get_art_photo_parameters():
-    return art_photo_parameters
+photograph_template = '''\
+{{{{Photograph
+ |photographer       = {photographer}
+ |title              = {title}
+ |description        = {description}
+ |depicted people    = {depicted_people}
+ |depicted place     = {depicted_place}
+ |date               = {date}
+ |medium             = {medium}
+ |dimensions         = {dimensions}
+ |institution        = {institution}
+ |department         = {department}
+ |references         = {references}
+ |object history     = {object_history}
+ |exhibition history = {exhibition_history}
+ |credit line        = {credit_line}
+ |inscriptions       = {inscriptions}
+ |notes              = {notes}
+ |accession number   = {accession_number}
+ |source             = {source}
+ |permission         = {permission}
+ |other versions     = {other_versions}
+ |wikidata           = {wikidata}
+ |camera coord       = {camera_coord}
 
-
-def fill_art_photo_template(parameters):
-    return art_photo_template.format(**parameters)
+}}}}
+'''
