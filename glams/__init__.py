effd3a9c523b19c92b30b02005e6bab6a5bf924d
infobliss <sid.nitdurgapur@gmail.com>
2017-08-19 21:49:38 +0530

    Generated glam_list dynamically

--- glams/__init__.py
+++ glams/__init__.py
@@ -1,4 +1,11 @@
 import pkgutil
 
+glam_list = []
+
+def register_glam(c):
+	glam_list.append(c)
+	return c
+	
+
 for loader, module_name, is_pkg in  pkgutil.walk_packages(__path__):
     module = loader.find_module(module_name).load_module(module_name)
